2627677ddada74dce2db73739e1c7fbe621cd46e
Iaroslav <Iaroslav.aleksandrovich.beldin@gmail.com>
2023-04-30 23:06:54 +0400

    Initial Commit

---
+++ AccountManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AccountManager
+//
+//  Created by Iaroslav Beldin on 30.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
